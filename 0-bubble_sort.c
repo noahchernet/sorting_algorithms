46225ed34711166479620adb8a2cddc09bde67c2
Noah Chernet <noahchernet@yahoo.com>
2022-01-09 23:18:43 +0300

    Reverted mistake (see commit e82ab3f7)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,14 +13,14 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int c;
 
-	for (j = 0; j < size; j++)
+	for (j = 0; j < size && size > 2; j++)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i + 1] < array[i])
 			{
-				c = array[j];
-				array[j] = array[i];
+				c = array[i + 1];
+				array[i + 1] = array[i];
 				array[i] = c;
 				print_array(array, size);
 			}
