e82ab3f7b3411d202585c59f5c56ac5e9e7a7f7a
Noah Chernet <noahchernet@yahoo.com>
2022-01-09 19:21:15 +0300

    Done task 0, designed a bubble sort algorithm. 0-O shows the best, average and worst case scenario time complexity (consecutively)

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts @array using the bubble sort algorithm, printing the
+ * array on each swap
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int c;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i + 1] < array[i])
+			{
+				c = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = c;
+				print_array(array, size);
+			}
+		}
+	}
+}
